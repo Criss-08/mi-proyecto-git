1511c55092f2e0f5d565c2a803576113f8bc402b
Cristian Escalante <criss.escalante08@gmail.com>
2025-07-04 08:37:53 -0300

    Agrego script verifica estado y arranque git-arrancar.sh

---
+++ git-arrancar.sh
@@ -0,0 +1,12 @@
+cd ~/mi-proyecto-git || exit
+
+echo "🌀 Bienvenido, arrancando Git..." 
+
+
+git status
+
+
+echo "🔁 Verificando si hay actualizaciones en el remoto..."
+git pull
+
+echo "✅ Todo listo para trabajar."
